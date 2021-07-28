9b88a20cdeb9b79229583e6b94e398e96cfd4041
Alex MacLure <almaclure@gmail.com>
2021-07-28 17:33:14 +0000

    Created client.js, modified play.js for ES6 Shorthand syntax

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  //console.logs data recieved from server ("data" is an event, there are multiple events eg "close", "connect", "data" etc...)
+  conn.on("data", (data) => {
+    console.log(data)
+  })
+  return conn;
+};
+
+module.exports =  {connect};
+//module.exports =  connect;
--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //console.logs data recieved from server ("data" is an event, there are multiple events eg "close", "connect", "data" etc...)
-  conn.on("data", (data) => {
-    console.log(data)
-  })
-  return conn;
-};
+//Importing using ES6 Shorthand syntax
+const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
