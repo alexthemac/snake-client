0dac06abbc09c9d9d66a17c145500543bb0ffe89
Alex MacLure <almaclure@gmail.com>
2021-07-28 22:47:03 +0000

    added handleUserInput function

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 //Importing using ES6 Shorthand syntax
 const {connect} = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+//this does not do anything extra for some reason...
+const handleUserInput = function (key) {
+  if(key === '\u0003') {
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+
